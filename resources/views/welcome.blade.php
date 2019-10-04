9e5b24a1e905c57842d92bd78dd0126b2c9af781
KILLforSKILL89 <ludek077@gmail.com>
2019-10-04 23:05:46 +0200

    poprawki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="carousel carousel-slider hide-on-small-only" style='margin:0; height:100vh;'>
+    <div class="carousel carousel-slider hide-on-small-only" style='margin:0; height:93vh;'>
         <a class='carousel-item'><img src='/photos/foto-1.jpg' style='height:100%;'/></a>
         <a class='carousel-item'><img src='/photos/foto-2.jpg' style='height:100%;'/></a>
         <a class='carousel-item'><img src='/photos/foto-3.jpg' style='height:100%;'/></a>
         <a class='carousel-item'><img src='/photos/foto-4.jpg' style='height:100%;'/></a>
         <a class='carousel-item'><img src='/photos/foto-5.jpg' style='height:100%;'/></a>
-        <a class='carousel-item'><img src='/photos/foto-7.jpg' style='height:100%;'/></a>
 
         <div class='carousel-fixed-item center hide-on-med-and-down'>
             <div class='row' style='font-size:2em; color:white; z-index:2;'>
